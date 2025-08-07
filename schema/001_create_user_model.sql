1c922ec7316b123079fafdc1c8b9a6ace621f26d
Riley O'Shannessy <rileyoshannessy1@gmail.com>
2025-08-07 19:51:58 +1000

    add sql files

---
+++ schema/001_create_user_model.sql
@@ -0,0 +1,6 @@
+CREATE TABLE tbl_user(
+    id INTEGER PRIMARY KEY,
+    usr_guid UUID,
+    usr_first_name NVARCHAR(200),
+    usr_last_name NVARCHAR(200)
+);
---
+++ schema/002_create_user_model.sql
@@ -0,0 +1,5 @@
+CREATE TABLE tbl_process (
+    id INTEGER PRIMARY KEY,
+    proc_guid UUID,
+    name NVARCHAR(200)
+);
